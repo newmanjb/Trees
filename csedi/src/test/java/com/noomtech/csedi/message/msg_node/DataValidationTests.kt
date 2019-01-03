4328e2660e6085edb2ffe2ee7fdbfeccd4e0e4e8
Joshua Newman <joshuajmnewman@gmail.com>
2019-01-03 15:31:01 +0000

    Update DataValidationTests.kt
    Added a unit test for the invalid characters in the TEXT type

--- csedi/src/test/java/com/noomtech/csedi/message/msg_node/DataValidationTests.kt
+++ csedi/src/test/java/com/noomtech/csedi/message/msg_node/DataValidationTests.kt
@@ -1,5 +1,6 @@
 package com.noomtech.csedi.message.msg_node
 
+import com.noomtech.csedi.message.parsers.Constants
 import com.noomtech.csedi.message.parsers.msgdata.MsgNode
 import com.noomtech.csedi.message.parsers.msgdata.MsgNodeValue
 import com.noomtech.csedi.message.parsers.msgschema.SchemaNode
@@ -16,25 +17,34 @@ class DataValidationTests {
     @Test
     fun testTextType() {
         val textNode = SchemaNode("text", false, SchemaNodeValueType.TEXT)
-        assert(textNode.validate(MsgNode("joshua")))
-        assert(textNode.validate(MsgNode("")))
+        assert(textNode.validate(MsgNode("text", MsgNodeValue("joshua"))))
+        assert(textNode.validate(MsgNode("text", MsgNodeValue(""))))
+
+        assert(!textNode.validate(MsgNode("text", MsgNodeValue("joshua" + Constants.NEWLINE))))
+        assert(!textNode.validate(MsgNode("text", MsgNodeValue("joshua" + "\t"))))
+        assert(!textNode.validate(MsgNode("text", MsgNodeValue(" "))))
+        assert(!textNode.validate(MsgNode("text", MsgNodeValue("   "))))
+        assert(!textNode.validate(MsgNode("text", MsgNodeValue("\t " + Constants.NEWLINE))))
+        assert(!textNode.validate(MsgNode("text", MsgNodeValue("jos\tu " + Constants.NEWLINE + "a"))))
+        assert(!textNode.validate(MsgNode("text", MsgNodeValue("joshu" + Constants.NEWLINE + "a"))))
+
     }
 
     @Test
     fun testNumberType() {
-        val textNode = SchemaNode("number", false, SchemaNodeValueType.NUMBER)
-        assert(textNode.validate(MsgNode("number", MsgNodeValue("1234"))))
-        assert(!textNode.validate(MsgNode("number", MsgNodeValue(""))))
-        assert(!textNode.validate(MsgNode("number", MsgNodeValue("1234a"))))
+        val numberNode = SchemaNode("number", false, SchemaNodeValueType.NUMBER)
+        assert(numberNode.validate(MsgNode("number", MsgNodeValue("1234"))))
+        assert(!numberNode.validate(MsgNode("number", MsgNodeValue(""))))
+        assert(!numberNode.validate(MsgNode("number", MsgNodeValue("1234a"))))
     }
 
     @Test
     fun testDecimalType() {
-        val textNode = SchemaNode("decimal", false, SchemaNodeValueType.DECIMAL)
-        assert(textNode.validate(MsgNode("decimal",MsgNodeValue("1234"))))
-        assert(textNode.validate(MsgNode("decimal",MsgNodeValue("1234.6789"))))
-        assert(!textNode.validate(MsgNode("decimal",MsgNodeValue(""))))
-        assert(!textNode.validate(MsgNode("decimal",MsgNodeValue("1234a"))))
-        assert(!textNode.validate(MsgNode("decimal",MsgNodeValue("1234.546456a"))))
+        val decimalNode = SchemaNode("decimal", false, SchemaNodeValueType.DECIMAL)
+        assert(decimalNode.validate(MsgNode("decimal",MsgNodeValue("1234"))))
+        assert(decimalNode.validate(MsgNode("decimal",MsgNodeValue("1234.6789"))))
+        assert(!decimalNode.validate(MsgNode("decimal",MsgNodeValue(""))))
+        assert(!decimalNode.validate(MsgNode("decimal",MsgNodeValue("1234a"))))
+        assert(!decimalNode.validate(MsgNode("decimal",MsgNodeValue("1234.546456a"))))
     }
 }
